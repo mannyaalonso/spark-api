9b4020fce71d080d8f65cec6849704bf3f0595d6
mannyaalonso <mannyaalonso@gmail.com>
2023-04-04 12:54:37 -0400

    app py delete commented out code

--- app.py
+++ app.py
@@ -30,10 +30,6 @@ app.config["SESSION_TYPE"] = "filesystem"
 app.config['JWT_SECRET_KEY'] = SECRET_KEY
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=1)
 
-# app.config["SESSION_PERMANENT"] = False
-# app.config["SESSION_TYPE"] = "filesystem"
-# app.config['JWT_SECRET_KEY'] = SECRET_KEY
-# app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=1)
 # app.config["MONGODB_SETTINGS"] = {'DB': "spark", "host": MONGO_URI}
 # app.config['CORS_HEADERS'] = 'Content-Type'
 
